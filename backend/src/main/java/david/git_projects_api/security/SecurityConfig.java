7d7651c56e297c9b1c2089a7f599814b034267bc
David Aslan <davidaslan112671@live.se>
2025-10-22 20:34:54 +0200

    allowed options on preflight

--- backend/src/main/java/david/git_projects_api/security/SecurityConfig.java
+++ backend/src/main/java/david/git_projects_api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package david.git_projects_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,7 +23,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // <--- allow preflight
+                        .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
