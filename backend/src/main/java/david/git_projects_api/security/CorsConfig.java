8cd4e3276f3f0910f10ea523e7368c2495439281
David Aslan <davidaslan112671@live.se>
2025-11-10 11:57:28 +0100

    hardcoded origins

--- backend/src/main/java/david/git_projects_api/security/CorsConfig.java
+++ backend/src/main/java/david/git_projects_api/security/CorsConfig.java
@@ -16,16 +16,13 @@ public class CorsConfig {
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
-    @Value("${FrontendUrl}")
-    private String frontendUrl;
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource(Environment environment) {
         CorsConfiguration config = new CorsConfiguration();
         // DO NOT use "*" if you will allow credentials. Use exact origin(s).
         // Always allow production frontend
         config.setAllowedOrigins(Arrays.asList(
-                frontendUrl
+                "https://git-projects-api.vercel.app"
         ));
 
         // Only add localhost origins in dev mode
