e2d839d1c0101723bfaba1353ff9ea68c5e07d06
David Aslan <davidaslan112671@live.se>
2025-11-14 14:23:12 +0100

    fancy api budget chart

--- svelteFrontend/src/routes/+page.svelte
+++ svelteFrontend/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
+	import ChartCounter from '$lib/components/ui/chart/ChartCounter.svelte';
+	import type { ApiKey } from '../types/AllTypes.js';
 
 	export let data;
-	const { apiKey } = data;
+	const { apiKey }: { apiKey: ApiKey } = data;
 </script>
 
 <div class="grid grid-cols-4 gap-4 text-white">
@@ -10,7 +12,7 @@
 		<Container>No API key available</Container>
 	{:else}
 		<Container class="col-span-2"><p>Api key: {apiKey.key}</p></Container>
-		<Container class="col-span-2"><p>Api key: {apiKey.key}</p></Container>
+		<Container class="col-span-2"><ChartCounter count={5} budget={10} /></Container>
 		<Container class="col-span-2"><p>Remaining api calls {apiKey.quota}</p></Container>
 		<Container class="col-span-4 row-span-2">
 			<p>Api key usage timeline graph</p>
