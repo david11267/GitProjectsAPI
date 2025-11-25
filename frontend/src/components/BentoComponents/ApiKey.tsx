3820e09b0a9d00482509a79f5c874735e6f4e044
David Aslan <David.aslan1999@gmail.com>
2025-11-25 09:30:03 +0100

    removed react frontend

--- frontend/src/components/BentoComponents/ApiKey.tsx
+++
@@ -1,8 +0,0 @@
-import { useUserApiKey } from "@/hooks/useUserApiKey";
-
-export default function ApiKey() {
-  const { status, data } = useUserApiKey();
-
-  if (!data) return <div>{status}</div>;
-  return <div>Api key: {data}</div>;
-}
