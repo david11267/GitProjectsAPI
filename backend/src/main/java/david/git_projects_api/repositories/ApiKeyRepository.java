10dd89fb6516ee56a8eda570f41d05b15c9f4e98
David Aslan <davidaslan112671@live.se>
2025-12-09 09:39:38 +0100

    removed unused files

--- backend/src/main/java/david/git_projects_api/repositories/ApiKeyRepository.java
+++ backend/src/main/java/david/git_projects_api/repositories/ApiKeyRepository.java
@@ -5,15 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 import java.util.UUID;
 
 public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
-    List<ApiKey> findApiKeyById(UUID id);
-
-    ApiKey findDistinctById(UUID id);
-
     ApiKey findDistinctByKey(UUID key);
 
     @Modifying
